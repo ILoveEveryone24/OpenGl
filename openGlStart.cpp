63213b003fb035ac6916c88fcb1961e1143a9316
ILoveEveryone24 <ernestascizas2404@gmail.com>
2022-12-14 09:40:39 +0100

    vertexShaders

--- openGlStart.cpp
+++ openGlStart.cpp
@@ -3,6 +3,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char *vertexShaderSource = "#version 460 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f)\n"
+  "}\0";//Vertex Shader
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) //This function will be called everytime a resize of a window happens
 {
@@ -44,6 +50,14 @@ int main()
   unsigned int VBO; //Defining vertex buffer object
   glGenBuffers(1, &VBO); //Generating buffer with 2 parameters, amount of buffers, and reference to a buffer
   glBindBuffer(GL_ARRAY_BUFFER, VBO); //Binding Vertex buffer object with GL_ARRAY_BUFFER
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Copies data (vertices) to currently bound buffer. Args (Where to copy data to, size of data in bytes, data we want to send, how we want the graphics card to manage data)
+
+  unsigned int vertexShader; //Creating vertexShader, where the vertex shader will be stored
+  vertexShader = glCreateShader(GL_VERTEX_SHADER); //Create the vertex shader, the GL_VERTEX_SHADER specifies what kind of shader we want to create
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //Attaching the vertex shader. Args(Where to attach, how many strings as source code, what to attach, NULL)
+  glCompileShader(vertexShader);
 
   while(!glfwWindowShouldClose(window)) //glfwWindowShouldClose is true when an attempt of closing a window happens
   {
